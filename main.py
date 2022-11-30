c311039c5d0bcdace6c2fc002956b4a6c10e24fa
Serhii <104906557+Lion6477@users.noreply.github.com>
2022-11-30 15:06:46 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import pygame
+
+pygame.init()
+WIDTH, HEIGHT = 400, 600
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption("My Game")
+
+clock = pygame.time.Clock()
+
+bound = 5
+c2s = 30
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+
+def drawWindow():
+    win.fill(black)
+    pygame.draw.rect(win, white, (0, 0, WIDTH, bound))  # up
+    pygame.draw.rect(win, white, (0, 0, bound, HEIGHT))  # left
+    pygame.draw.rect(win, white, (WIDTH - bound, 0, bound, HEIGHT))  # right
+    # pygame.draw.rect(win, white, (?, ?, ?, ?))  # down
+    pygame.display.update()
+
+
+run = True
+
+while run:
+    clock.tick(c2s)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    drawWindow()
+
+pygame.quit()
